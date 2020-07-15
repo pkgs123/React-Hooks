1de100a44c337534846aa9f3cf89527525caff62
Prashant Singh <prashant.55555@yahoo.co.in>
2020-07-15 19:43:45 +0530

    Basic React App using Material UI

--- src/App.js
+++ src/App.js
@@ -1,26 +1,56 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import { Grid, Hidden } from '@material-ui/core';
+import AppBar from '@material-ui/core/AppBar';
+import { makeStyles, withStyles } from '@material-ui/core/styles';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const useStyles = ((theme) => ({
+    root: {
+        flexGrow: 1,
+    },
+    // menuButton: {
+    //   marginRight: theme.spacing(2),
+    // },
+    title: {
+        flexGrow: 1,
+    },
+}));
+
+
+class App extends Component {
+
+    render() {
+        const classes = useStyles();
+        return (
+            <>
+                <Grid container>
+                    <Hidden smUp>
+                        <Grid item xs={12}>
+                            <Grid item xs={8}>
+                                1st Grid Item
+                            </Grid>
+                            <Grid item xs={4}>
+                                2nd Grid Item
+                            </Grid>
+                            Hi Prashant
+                   <div className={classes.root}>
+                                <AppBar position="static">
+                                    <Toolbar>
+                                    </Toolbar>
+                                </AppBar>
+                            </div>
+                        </Grid>
+                    </Hidden>
+                    <Hidden xsDown>
+                        <Grid item sm={12} md={12} lg={12}>
+                            Thanks Priyanka
+                   </Grid>
+                   
+                    </Hidden>
+                </Grid>
+            </>
+        )
+    }
 }
-
-export default App;
+export default withStyles(useStyles)(App);
